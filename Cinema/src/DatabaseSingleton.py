e63d420664ecda2ba14f53fb73c86220bc71f351
Sharkpb8 <sharkpb8@gmail.com>
2025-01-15 14:25:11 +0100

    connection pool

--- Cinema/src/DatabaseSingleton.py
+++ Cinema/src/DatabaseSingleton.py
@@ -1,19 +1,25 @@
 import mysql.connector
+from mysql.connector import pooling
 import json
 
 class DatabaseSingleton:
     conn = None
     isolation_level = "REPEATABLE READ"
+    active_connections = []
 
     def __new__(cls):
         if not cls.conn:
             cls.new_conn()
-        cls.set_session_isolation_level()
-        return cls.conn
+        connection = cls.conn.get_connection()
+        cls.set_session_isolation_level(connection)
+        cls.active_connections.append(connection)
+        return connection
     
     @classmethod
     def new_conn(cls):
-        connection = mysql.connector.connect(
+        connection = pooling.MySQLConnectionPool(
+        pool_name="Connection_pool",
+        pool_size=2,
         host=cls.readconfig("host"),
         port=cls.readconfig("port"),
         user=cls.readconfig("user"),
@@ -25,7 +31,8 @@ class DatabaseSingleton:
     @classmethod
     def close_conn(cls):
         if(cls.conn):
-            cls.conn.close()
+            for i in cls.active_connections:
+                i.close()
             cls.conn = None
 
     @classmethod
@@ -35,9 +42,9 @@ class DatabaseSingleton:
             return config["database"][key]
     
     @classmethod
-    def set_session_isolation_level(cls):
+    def set_session_isolation_level(cls,connection):
         sql = f"SET SESSION TRANSACTION ISOLATION LEVEL {cls.isolation_level};"
-        cursor = cls.conn.cursor()
+        cursor = connection.cursor()
         cursor.execute(sql)
     
     @classmethod
